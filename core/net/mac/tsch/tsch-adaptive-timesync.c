bef0db042e2667e4c8d624005400744bbe379f08
Simon Duquennoy <simon.duquennoy@inria.fr>
2016-09-23 09:54:10 +0200

    Latest adaptive sync fixes

--- core/net/mac/tsch/tsch-adaptive-timesync.c
+++ core/net/mac/tsch/tsch-adaptive-timesync.c
@@ -93,11 +93,16 @@ static void
 timesync_learn_drift_ticks(uint32_t time_delta_asn, int32_t drift_ticks)
 {
   /* should fit in 32-bit unsigned integer */
-  uint32_t time_delta_ticks = time_delta_asn * tsch_timing[tsch_ts_timeslot_length];
+  int32_t time_delta_ticks = time_delta_asn * tsch_timing[tsch_ts_timeslot_length];
   int32_t real_drift_ticks = drift_ticks + compensated_ticks;
   int32_t last_drift_ppm = (int32_t)((int64_t)real_drift_ticks * TSCH_DRIFT_UNIT / time_delta_ticks);
 
   drift_ppm = timesync_entry_add(last_drift_ppm, time_delta_ticks);
+
+  TSCH_LOG_ADD(tsch_log_message,
+      snprintf(log->message, sizeof(log->message),
+          "drift %ld (%ld)", drift_ppm / 256, drift_ppm ));
+
 }
 /*---------------------------------------------------------------------------*/
 /* Either reset or update the neighbor's drift */
