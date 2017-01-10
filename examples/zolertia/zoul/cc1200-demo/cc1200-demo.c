0fee7315001e9ed19f75121a97944e4a977256b3
Simon Duquennoy <simon.duquennoy@gmail.com>
2017-01-10 08:51:18 +0100

    wip

--- examples/zolertia/zoul/cc1200-demo/cc1200-demo.c
+++ examples/zolertia/zoul/cc1200-demo/cc1200-demo.c
@@ -92,23 +92,31 @@ PROCESS_THREAD(cc1200_demo_process, ev, data)
   radio_tx_mode &= ~RADIO_TX_MODE_SEND_ON_CCA;
   NETSTACK_RADIO.set_value(RADIO_PARAM_TX_MODE, radio_tx_mode);
 
-  /*if(linkaddr_node_addr.u8[0] == 180 && linkaddr_node_addr.u8[1] == 73) {*/ // NODE 16
-  /*if(linkaddr_node_addr.u8[0] == 177 && linkaddr_node_addr.u8[1] == 53) {*/ // NODE 17
-  /*if(linkaddr_node_addr.u8[0] == 182 && linkaddr_node_addr.u8[1] == 20) {*/ // NODE 1
-     if(linkaddr_node_addr.u8[0] == 178 && linkaddr_node_addr.u8[1] == 21) { // NODE 18
-  //if(linkaddr_node_addr.u8[1] == 0xcc) {
-    while(1) {
-      PROCESS_YIELD();
-      if(ev == PROCESS_EVENT_TIMER) {
-        //printf("Broadcast --> %u [%u %u]\n", (unsigned)counter, linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+  while(1) {
+    PROCESS_YIELD();
+    if(ev == PROCESS_EVENT_TIMER) {
+      //printf("Broadcast --> %u [%u %u]\n", (unsigned)counter, linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+      printf("Address [%u %u]\n", linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1]);
+      //if(linkaddr_node_addr.u8[0] == 182 && linkaddr_node_addr.u8[1] == 20) { // NODE 1
+      //if(linkaddr_node_addr.u8[0] == 178 && linkaddr_node_addr.u8[1] == 3) { // NODE 12
+      if(linkaddr_node_addr.u8[0] == 182 && linkaddr_node_addr.u8[1] == 13) { // NODE 13
+      //if(linkaddr_node_addr.u8[0] == 181 && linkaddr_node_addr.u8[1] == 102) { // NODE 14
+      //if(linkaddr_node_addr.u8[0] == 181 && linkaddr_node_addr.u8[1] == 138) { // NODE 15
+      //if(linkaddr_node_addr.u8[0] == 180 && linkaddr_node_addr.u8[1] == 73) { // NODE 16
+      //if(linkaddr_node_addr.u8[0] == 177 && linkaddr_node_addr.u8[1] == 53) { // NODE 17
+      //if(linkaddr_node_addr.u8[0] == 178 && linkaddr_node_addr.u8[1] == 21) { // NODE 18
+      //if(linkaddr_node_addr.u8[0] == 181 && linkaddr_node_addr.u8[1] == 252) { // NODE 19
+      //if(linkaddr_node_addr.u8[0] == 177 && linkaddr_node_addr.u8[1] == 168) { // NODE 20
+      //if(linkaddr_node_addr.u8[0] == 177 && linkaddr_node_addr.u8[1] == 106) { // NODE 21
+      //if(linkaddr_node_addr.u8[1] == 0xcc) {
         printf("Broadcast --> %u\n", (unsigned)counter);
         leds_toggle(LEDS_RED);
         //packetbuf_copyfrom(&counter, sizeof(counter));
         //broadcast_send(&bc);
         NETSTACK_RADIO.send(&counter, 4);
         counter++;
-        etimer_set(&et, LOOP_INTERVAL);
       }
+      etimer_set(&et, LOOP_INTERVAL);
     }
   }
 
